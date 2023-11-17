9b734354e2a68b9bd67d7112e9f21ad0d960aff5
Sirine Yousfi <sirineyousfi@yahoo.fr>
2023-11-17 11:12:54 +0100

    update sign

---
+++ includes/sign.php
@@ -0,0 +1,25 @@
+<!-- sign.php -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+    <link rel="stylesheet" href="assets/css/sign.css">
+</head>
+<body>
+
+<div class="login-container">
+    <h2>Sign In</h2>
+    <form action="" method="post"> 
+        <input type="text" name="username" placeholder="Username" required>
+        <br>
+        <input type="password" name="password" placeholder="Password" required>
+        <br>
+        <button type="submit">Sign In</button>
+    </form>
+</div>
+
+</body>
+</html>
